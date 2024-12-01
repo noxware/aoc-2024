4e62806e7cd4d88c46057673b4253c44d20d4143
noxware <7684329+noxware@users.noreply.github.com>
2024-12-01 18:29:37 -0300

    day 1 part 1

--- src/bin/d1p1.rs
+++ src/bin/d1p1.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../../inputs/day1.txt");
+
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in input.lines().filter(|l| !l.is_empty()) {
+        let mut split = line.split_whitespace();
+        left.push(split.next().unwrap().parse::<i32>().unwrap());
+        right.push(split.next().unwrap().parse::<i32>().unwrap());
+    }
+
+    left.sort();
+    right.sort();
+
+    let distances = left.iter().zip(right.iter()).map(|(l, r)| (l - r).abs());
+    let sum = distances.sum::<i32>();
+    println!("{}", sum);
 }
