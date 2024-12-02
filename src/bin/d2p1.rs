a2f137b3e0c7bf774b9e49994651672c70491e76
noxware <7684329+noxware@users.noreply.github.com>
2024-12-02 03:18:48 -0300

    day 2 part 1

---
+++ src/bin/d2p1.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let input = include_str!("../../inputs/day2.txt");
+
+    let reports = input.split('\n').filter(|l| !l.is_empty());
+    let safe = reports.filter(|l| {
+        let levels = l
+            .split_whitespace()
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+
+        let are_levels_incresing = levels.windows(2).all(|w| w[1] > w[0]);
+
+        levels.windows(2).all(|w| {
+            let is_window_incresing = w[1] > w[0];
+            let distance = (w[1] - w[0]).abs();
+
+            distance >= 1 && distance <= 3 && is_window_incresing == are_levels_incresing
+        })
+    });
+
+    println!("{}", safe.count())
+}
